d6591a6a4fc7f376cd7715a31260444b8c2cb144
Vladislav Sas <vladislavsasweb@gmail.com>
2024-08-28 21:41:16 +0300

    start

---
+++ todoapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField('Названия задания', max_length=500)
+    is_complete = models.BooleanField('Выполнено', default=False)
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+
+    def __str__(self):
+        return self.title
+
