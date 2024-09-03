e272482c861b36db4f7d83f582bc51aa23b39c27
Vladislav Sas <vladislavsasweb@gmail.com>
2024-09-03 21:49:34 +0300

    priority

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import ToDo, Category
 from .forms import ToDoForm
-from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from datetime import datetime
 
 def index(request):
     query = request.GET.get('q')
     selected_categories = request.GET.getlist('categories')
+    selected_deadline = request.GET.get('deadline')
 
-    # Сортировка по дате создания в убывающем порядке
+    # Фильтрация пустых значений из selected_categories
+    selected_categories = [cat for cat in selected_categories if cat.isdigit()]
+
     todos = ToDo.objects.all().order_by('-created_at')
 
     if query:
@@ -17,21 +20,43 @@ def index(request):
     if selected_categories:
         todos = todos.filter(categories__id__in=selected_categories).distinct()
 
-    categories = Category.objects.all()  # Получение всех категорий для отображения в форме
+    if selected_deadline:
+        try:
+            deadline_date = datetime.strptime(selected_deadline, '%Y-%m-%d').date()
+            todos = todos.filter(deadline__lte=deadline_date)
+        except ValueError:
+            # Если формат даты неверный, просто не применяем фильтр
+            pass
+
+    categories = Category.objects.all()
     return render(request, 'todoapp/index.html', {
         'todo_list': todos,
         'title': 'Главная страница',
         'categories': categories,
         'selected_categories': selected_categories,
+        'selected_deadline': selected_deadline,
     })
 
+
 @require_http_methods(['POST'])
-@csrf_exempt
 def add(request):
     form = ToDoForm(request.POST)
     if form.is_valid():
-        form.save()
-    return redirect('index')
+        todo = form.save(commit=False)
+        todo.save()  # Сначала сохраняем задачу
+        form.save_m2m()  # Затем сохраняем многие ко многим поля (категории)
+        return redirect('index')
+    else:
+        todos = ToDo.objects.all().order_by('-created_at')
+        categories = Category.objects.all()
+        return render(request, 'todoapp/index.html', {
+            'todo_list': todos,
+            'form': form,
+            'title': 'Главная страница',
+            'categories': categories,
+            'selected_categories': request.GET.getlist('categories'),
+        })
+
 
 def update(request, todo_id):
     todo = get_object_or_404(ToDo, id=todo_id)
@@ -39,10 +64,12 @@ def update(request, todo_id):
     todo.save()
     return redirect('index')
 
+
 def delete(request, todo_id):
     todo = get_object_or_404(ToDo, id=todo_id)
     todo.delete()
     return redirect('index')
+
 
 def edit(request, todo_id):
     todo = get_object_or_404(ToDo, id=todo_id)
@@ -51,20 +78,12 @@ def edit(request, todo_id):
         if form.is_valid():
             form.save()
             return redirect('index')
-        else:
-            return render(request, 'todoapp/index.html', {
-                'form': form,
-                'todo': todo,
-                'error': 'Исправьте ошибки в форме.',
-                'categories': Category.objects.all(),
-            })
     else:
         form = ToDoForm(instance=todo)
-        categories = Category.objects.all()
-    return render(request, 'todoapp/index.html', {
+    return render(request, 'todoapp/edit.html', {
         'form': form,
         'todo': todo,
-        'categories': categories,
+        'categories': Category.objects.all(),
     })
 
 
